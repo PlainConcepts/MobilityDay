77ca36fc40c65d0eeddb8dd39797cb2b3f1f45b1
Daniel Cáceres Maña <dcaceres@plainconcepts.com>
2015-05-05 11:44:50 +0200

    Deleted native projects.

--- Xamarin Demos/MobilityDay.Native/MobilityDay.Droid/Bootstrap/WebBrowserPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace MobilityDay.Droid.Bootstrap
-{
-    public class WebBrowserPluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.WebBrowser.PluginLoader>
-    {
-    }
-}
